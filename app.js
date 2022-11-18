73a66dc05f0ece47cf8320a8049b54b0923d9f95
programmerP22 <chiangcw19@gmail.com>
2022-11-19 01:40:21 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
@@ -8,10 +11,17 @@ const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 
 app.get('/', (req, res) => {
@@ -27,9 +37,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
